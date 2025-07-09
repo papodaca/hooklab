00d1b30eb51eb503c0d8b13c4e041631b23bf5fa
Ethan Apodaca <papodaca@gmail.com>
2025-07-09 03:07:46 -0700

    Fix some of the event details

--- Frontend/Components/MainContent/RequestDetails.svelte
+++ Frontend/Components/MainContent/RequestDetails.svelte
@@ -15,21 +15,74 @@
 
 <div class="p-4">
   <div class="flex flex-wrap -mx-3">
+    <div
+      class="w-full px-3 mb-6 {params && Object.keys(params).length > 0
+        ? 'md:w-1/2'
+        : ''}"
+    >
+      <h3
+        class="text-lg font-semibold text-gray-900 dark:text-white mb-2 border-b border-gray-200 dark:border-gray-700 pb-2"
+      >
+        Headers
+      </h3>
+      <div class="overflow-x-auto">
+        <table
+          class="min-w-full text-sm text-left text-gray-500 dark:text-gray-400"
+        >
+          <thead
+            class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+          >
+            <tr>
+              <th scope="col" class="px-4 py-2">Key</th>
+              <th scope="col" class="px-4 py-2">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each Object.entries(headers || {}).sort() as [key, value]}
+              <tr
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <th
+                  scope="row"
+                  class="px-4 py-2 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                  >{key}</th
+                >
+                <td class="px-4 py-2 font-mono">{value}</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    </div>
     {#if params && Object.keys(params).length > 0}
       <div class="w-full md:w-1/2 px-3 mb-6">
-        <h3 class="text-lg font-semibold text-gray-900 dark:text-white mb-2 border-b border-gray-200 dark:border-gray-700 pb-2">Query Parameters</h3>
+        <h3
+          class="text-lg font-semibold text-gray-900 dark:text-white mb-2 border-b border-gray-200 dark:border-gray-700 pb-2"
+        >
+          Query Parameters
+        </h3>
         <div class="overflow-x-auto">
-          <table class="min-w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <table
+            class="min-w-full text-sm text-left text-gray-500 dark:text-gray-400"
+          >
+            <thead
+              class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+            >
               <tr>
                 <th scope="col" class="px-4 py-2">Key</th>
                 <th scope="col" class="px-4 py-2">Value</th>
               </tr>
             </thead>
             <tbody>
-              {#each Object.entries(params) as [key, value]}
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th scope="row" class="px-4 py-2 font-medium text-gray-900 dark:text-white whitespace-nowrap">{key}</th>
+              {#each Object.entries(params).sort() as [key, value]}
+                <tr
+                  class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <th
+                    scope="row"
+                    class="px-4 py-2 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                    >{key}</th
+                  >
                   <td class="px-4 py-2 font-mono">{value}</td>
                 </tr>
               {/each}
@@ -38,34 +91,17 @@
         </div>
       </div>
     {/if}
-
-    <div class="w-full md:w-1/2 px-3 mb-6">
-      <h3 class="text-lg font-semibold text-gray-900 dark:text-white mb-2 border-b border-gray-200 dark:border-gray-700 pb-2">Headers</h3>
-      <div class="overflow-x-auto">
-        <table class="min-w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-4 py-2">Key</th>
-              <th scope="col" class="px-4 py-2">Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each Object.entries(headers || {}) as [key, value]}
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-4 py-2 font-medium text-gray-900 dark:text-white whitespace-nowrap">{key}</th>
-                <td class="px-4 py-2 font-mono">{value}</td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
   </div>
 
   <div>
-    <h3 class="text-lg font-semibold text-gray-900 dark:text-white mb-2 border-b border-gray-200 dark:border-gray-700 pb-2">Body</h3>
+    <h3
+      class="text-lg font-semibold text-gray-900 dark:text-white mb-2 border-b border-gray-200 dark:border-gray-700 pb-2"
+    >
+      Body
+    </h3>
     {#if formattedBody}
-      <pre class="bg-gray-50 dark:bg-gray-900 p-4 rounded-md overflow-x-auto text-sm text-gray-800 dark:text-gray-200 font-mono">{formattedBody}</pre>
+      <pre
+        class="bg-gray-50 dark:bg-gray-900 p-4 rounded-md overflow-x-auto text-sm text-gray-800 dark:text-gray-200 font-mono">{formattedBody}</pre>
     {:else}
       <p class="text-gray-500 dark:text-gray-400">No body provided.</p>
     {/if}
